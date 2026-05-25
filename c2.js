b20a8b52f969f0792f6dca6d1c112039d472372c
iki1uc <entertrainer@web.de>
2026-05-25 19:15:47 +0200

    Update c2.js

--- c2.js
+++ c2.js
@@ -1,1 +1,12 @@
+// c2.js – Cube‑2 Logik (Struktur‑Cube)
+
+function c2_out(ist, soll) {
+  document.getElementById("out").innerHTML = `
+    <div class="out-title">Wissenschaftlicher Output – Cube‑2</div>
+    <div class="out-ist"><b>IST:</b> ${ist}</div>
+    <div class="out-soll"><b>SOLL:</b> ${soll}</div>
+  `;
+}
+
+c2_out("Struktur aktiv", "Bereit für Verbund");
 
